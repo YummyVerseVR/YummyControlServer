de801660486ce07ce6297f36a6518a5c883ddac4
uPiscium <upiscium@gmail.com>
2025-09-15 19:03:22 +0900

    Remove unused import

--- src/app.py
+++ src/app.py
@@ -1,7 +1,6 @@
 import smtplib
-import string
 import uuid
-from fastapi import FastAPI, APIRouter, HTTPException, params, status
+from fastapi import FastAPI, APIRouter, HTTPException, status
 from fastapi.responses import JSONResponse
 from dotenv import load_dotenv
 from email.mime.text import MIMEText
@@ -14,13 +13,16 @@ from typing import NamedTuple
 import requests
 from pydantic import BaseModel
 
+
 class DbStruct(NamedTuple):
     email: str
     qr: str
 
+
 class UserState(BaseModel):
     uuid: str
     is_ready: bool
+
 
 class UserRequest(BaseModel):
     email: str
@@ -29,19 +31,30 @@ class UserRequest(BaseModel):
 
 QR_API_URL = "http://localhost:8001/"
 
+
 class App:
     def __init__(self):
         self.app = FastAPI()
         self.router = APIRouter()
         self.__setup_routes()
         self.__setup_email()
-        #とりあえず仮で辞書型を。処理途中でサーバ落ちたら終わる
+        # とりあえず仮で辞書型を。処理途中でサーバ落ちたら終わる
         self.__db = {}
-    
+
     def __setup_routes(self):
         self.router.add_api_route("/", self.read_root)
-        self.router.add_api_route("/set-user-status", self.set_user_status, methods=["POST"], status_code=status.HTTP_200_OK)
-        self.router.add_api_route("/register-request", self.register_request, methods=["POST"], status_code=status.HTTP_200_OK)
+        self.router.add_api_route(
+            "/set-user-status",
+            self.set_user_status,
+            methods=["POST"],
+            status_code=status.HTTP_200_OK,
+        )
+        self.router.add_api_route(
+            "/register-request",
+            self.register_request,
+            methods=["POST"],
+            status_code=status.HTTP_200_OK,
+        )
 
     def get_app(self):
         self.app.include_router(self.router)
@@ -57,7 +70,7 @@ class App:
     async def read_root(self):
         return {"detail": "A Control Server"}
 
-    #Database ServerからNotify Readyを受け取る
+    # Database ServerからNotify Readyを受け取る
     async def set_user_status(self, item: UserState) -> JSONResponse:
         if item.uuid not in self.__db:
             print(f"now database: {self.__db}")
@@ -65,14 +78,17 @@ class App:
 
         if not item.is_ready:
             self.__db.pop(item.uuid)
-            return JSONResponse(content={"detail": "some thing wrong. please try again"}, status_code=400)
+            return JSONResponse(
+                content={"detail": "some thing wrong. please try again"},
+                status_code=400,
+            )
 
         await self.send_qr(item.uuid)
         return JSONResponse(content={"detail": "QR Code sent successfully"})
 
-    #GAS ServerからSend requestを受け取る
+    # GAS ServerからSend requestを受け取る
     async def register_request(self, item: UserRequest) -> JSONResponse:
-        #uuidを鍵としてemail, qrを保存
+        # uuidを鍵としてemail, qrを保存
         generated_uuid = str(uuid.uuid4())
 
         r = await self.request_gen_qr(generated_uuid, item.request)
@@ -81,42 +97,53 @@ class App:
         print(f"uuid : {generated_uuid}")
         print(f"request : {item.request}")
         if r["qr_code"] is None:
-            return JSONResponse(content={"detail": "Failed to generate QR code"}, status_code=500)
-        return JSONResponse(content={"detail": "User registered successfully"}, status_code=200)
+            return JSONResponse(
+                content={"detail": "Failed to generate QR code"}, status_code=500
+            )
+        return JSONResponse(
+            content={"detail": "User registered successfully"}, status_code=200
+        )
 
     async def request_gen_qr(self, uuid: str, request: str):
-        item = {
-            "uuid": uuid,
-            "request": request
-        }
-        r_post = requests.post(QR_API_URL+"gen-qr", json=item)
+        item = {"uuid": uuid, "request": request}
+        r_post = requests.post(QR_API_URL + "gen-qr", json=item)
         if r_post.status_code != 201:
             print(f"gen-qrとの接続に失敗しました: {r_post.text}")
             return {"uuid": uuid, "qr_code": None}
-        return {"uuid": r_post.json().get("uuid"), "qr_code": r_post.json().get("qr_code")}
+        return {
+            "uuid": r_post.json().get("uuid"),
+            "qr_code": r_post.json().get("qr_code"),
+        }
 
     async def send_qr(self, uuid: str):
         print(f"from: {self.from_email}")
         print(f"to: {self.__db[uuid]['email']}")
         print(f"QR Code (base64): {self.__db[uuid]['qr_code'][:30]}...")
         if not self.from_email or not self.app_password:
-            raise HTTPException(status_code=500, detail="Email configuration is not set")
+            raise HTTPException(
+                status_code=500, detail="Email configuration is not set"
+            )
         if uuid not in self.__db:
             raise HTTPException(status_code=404, detail="UUID not found")
-        
+
         try:
             msg = MIMEMultipart()
-            msg['From'] = formataddr(('YummyVerse[開発用]', self.from_email))
-            msg['Subject'] = 'YummyVerseのQRコード'
-            msg['Date'] = formatdate(localtime=True)
-            msg['To'] = self.__db[uuid]["email"]
+            msg["From"] = formataddr(("YummyVerse[開発用]", self.from_email))
+            msg["Subject"] = "YummyVerseのQRコード"
+            msg["Date"] = formatdate(localtime=True)
+            msg["To"] = self.__db[uuid]["email"]
 
-            body = MIMEText('YummyVerseのQRコードをお送りします。アプリで読み取ってください。', 'plain')
+            body = MIMEText(
+                "YummyVerseのQRコードをお送りします。アプリで読み取ってください。",
+                "plain",
+            )
             msg.attach(body)
 
             attachment = MIMEApplication(base64.b64decode(self.__db[uuid]["qr_code"]))
             file_name = f"YummyVerse_QR_{uuid}.png"
-            attachment.add_header('Content-Disposition', 'attachment', filename=file_name)
+            attachment.add_header(
+                "Content-Disposition", "attachment", filename=file_name
+            )
             msg.attach(attachment)
 
             with smtplib.SMTP_SSL(self.smtp_server, self.smtp_port) as server:
@@ -126,3 +153,4 @@ class App:
         except Exception as e:
             print(f"error: {e}")
             raise HTTPException(status_code=500, detail=str(e))
+
