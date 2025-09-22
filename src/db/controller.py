84ad3f13928b91b894d30994b142702edd602931
uPiscium <upiscium@gmail.com>
2025-09-22 23:43:22 +0900

    Add db features

---
+++ src/db/controller.py
@@ -0,0 +1,106 @@
+import shutil
+import os
+from typing import Any
+
+from db.model import UserData
+
+
+class DataBase:
+    def __init__(self, config: dict):
+        self.__config = config.get("db", {})
+        db_path = self.__config.get("path", "~/YummyVerse")
+        self.__db_path = os.path.expanduser(db_path)
+        self.__tables: dict[str, UserData] = {}
+
+        os.makedirs(self.__db_path, exist_ok=True)
+        self.__load_tables()
+
+    def __load_tables(self) -> None:
+        for user_id in os.listdir(self.__db_path):
+            try:
+                self.__tables[user_id] = UserData(user_id, self.__db_path)
+            except ValueError:
+                continue
+
+        for user_id, user_data in self.__tables.items():
+            if os.path.exists(user_data.get_meta_path()):
+                user_data.load_meta()
+
+            if os.path.exists(user_data.get_qr_path()):
+                user_data.set_status(UserData.QR_FILE, True)
+            if os.path.exists(user_data.get_image_path()):
+                user_data.set_status(UserData.IMAGE_FILE, True)
+            if os.path.exists(user_data.get_model_path()):
+                user_data.set_status(UserData.MODEL_FILE, True)
+            if os.path.exists(user_data.get_audio_path()):
+                user_data.set_status(UserData.AUDIO_FILE, True)
+            if os.path.exists(user_data.get_param_path()):
+                user_data.set_status(UserData.PARAM_FILE, True)
+
+    def get_user(self, user_id: str) -> UserData | None:
+        if user_id not in self.__tables.keys():
+            return None
+
+        return self.__tables[user_id]
+
+    def remove_user(self, user_id: str) -> bool:
+        if user_id not in self.__tables.keys():
+            return False
+
+        user_data = self.__tables[user_id]
+        user_data.remove_all_files()
+        shutil.rmtree(user_data.get_user_path(), ignore_errors=True)
+
+        del self.__tables[user_id]
+
+        return True
+
+    def is_exist(self, user_id: str) -> bool:
+        return user_id in self.__tables.keys()
+
+    def is_ready(self, user_id: str) -> bool:
+        if user_id not in self.__tables.keys():
+            return False
+
+        return self.__tables[user_id].is_ready()
+
+    def add_user(self, user_id: str):
+        if user_id in self.__tables.keys():
+            return self.__tables[user_id]
+
+        user_data = UserData(user_id, self.__db_path)
+
+        self.__tables[user_id] = user_data
+
+    def list_users(self) -> list[UserData]:
+        return list(self.__tables.values())
+
+    def load_qr(self, user_id: str, qr_data: Any) -> None:
+        if user_id not in self.__tables.keys():
+            raise ValueError(f"User {user_id} not found in database.")
+
+        self.__tables[user_id].load_qr(qr_data)
+
+    def load_image(self, user_id: str, image_data: Any) -> None:
+        if user_id not in self.__tables.keys():
+            raise ValueError(f"User {user_id} not found in database.")
+
+        self.__tables[user_id].load_image(image_data)
+
+    def load_model(self, user_id: str, model_data: Any) -> None:
+        if user_id not in self.__tables.keys():
+            raise ValueError(f"User {user_id} not found in database.")
+
+        self.__tables[user_id].load_model(model_data)
+
+    def load_audio(self, user_id: str, audio_data: Any) -> None:
+        if user_id not in self.__tables.keys():
+            raise ValueError(f"User {user_id} not found in database.")
+
+        self.__tables[user_id].load_audio(audio_data)
+
+    def load_param(self, user_id: str, param_data: dict) -> None:
+        if user_id not in self.__tables.keys():
+            raise ValueError(f"User {user_id} not found in database.")
+
+        self.__tables[user_id].load_param(param_data)
---
+++ src/db/model.py
@@ -0,0 +1,193 @@
+import shutil
+import os
+import json
+
+from fastapi import UploadFile
+from io import BytesIO
+from pydantic import BaseModel
+
+
+class MetaData(BaseModel):
+    uuid: str
+    email: str
+    qr_code: str
+    request: str
+
+
+class UserData:
+    META_FILE = "meta.json"
+    QR_FILE = "qr.png"
+    IMAGE_FILE = "image.png"
+    MODEL_FILE = "model.glb"
+    AUDIO_FILE = "audio.wav"
+    PARAM_FILE = "params.json"
+
+    def __init__(self, user_id: str, db_path: str):
+        self.__db_path = db_path
+        self.__uuid = user_id
+        self.__meta_path = os.path.join(self.get_user_path(), UserData.META_FILE)
+        self.__qr_path = os.path.join(self.get_user_path(), UserData.QR_FILE)
+        self.__image_path = os.path.join(self.get_user_path(), UserData.IMAGE_FILE)
+        self.__model_path = os.path.join(self.get_user_path(), UserData.MODEL_FILE)
+        self.__audio_path = os.path.join(self.get_user_path(), UserData.AUDIO_FILE)
+        self.__param_path = os.path.join(self.get_user_path(), UserData.PARAM_FILE)
+        self.__status = {
+            UserData.QR_FILE: False,
+            UserData.IMAGE_FILE: False,
+            UserData.MODEL_FILE: False,
+            UserData.AUDIO_FILE: False,
+            UserData.PARAM_FILE: False,
+        }
+
+        self.meta = MetaData(
+            uuid=self.__uuid,
+            email="",
+            qr_code="",
+            request="",
+        )
+
+        os.makedirs(self.get_user_path(), exist_ok=True)
+
+    def get_uuid(self) -> str:
+        return self.__uuid
+
+    def get_user_path(self) -> str:
+        return os.path.join(self.__db_path, str(self.__uuid))
+
+    def get_meta_path(self) -> str:
+        return self.__meta_path if os.path.exists(self.__meta_path) else ""
+
+    def get_qr_path(self) -> str:
+        return self.__qr_path if os.path.exists(self.__qr_path) else ""
+
+    def get_image_path(self) -> str:
+        return self.__image_path if os.path.exists(self.__image_path) else ""
+
+    def get_model_path(self) -> str:
+        return self.__model_path if os.path.exists(self.__model_path) else ""
+
+    def get_audio_path(self) -> str:
+        return self.__audio_path if os.path.exists(self.__audio_path) else ""
+
+    def get_param_path(self) -> str:
+        return self.__param_path if os.path.exists(self.__param_path) else ""
+
+    def set_status(self, file_type: str, status: bool) -> None:
+        if file_type in self.__status.keys():
+            self.__status[file_type] = status
+
+    def is_ready(self) -> bool:
+        return all(self.__status.values())
+
+    def remove_all_files(self) -> None:
+        if not os.path.exists(self.get_user_path()):
+            return
+
+        if os.path.exists(self.__meta_path):
+            os.remove(self.__meta_path)
+        if os.path.exists(self.__qr_path):
+            os.remove(self.__qr_path)
+        if os.path.exists(self.__image_path):
+            os.remove(self.__image_path)
+        if os.path.exists(self.__model_path):
+            os.remove(self.__model_path)
+        if os.path.exists(self.__audio_path):
+            os.remove(self.__audio_path)
+        if os.path.exists(self.__param_path):
+            os.remove(self.__param_path)
+
+        self.__status = {
+            UserData.QR_FILE: False,
+            UserData.IMAGE_FILE: False,
+            UserData.MODEL_FILE: False,
+            UserData.AUDIO_FILE: False,
+            UserData.PARAM_FILE: False,
+        }
+
+    def save_meta(self) -> None:
+        """
+        Save metadata to a JSON file.
+        """
+        with open(self.__meta_path, "w") as f:
+            json.dump(self.meta, f, indent=4)
+
+    def load_meta(self) -> None:
+        """
+        Load metadata from a JSON file.
+        """
+        if not os.path.exists(self.__meta_path):
+            return
+
+        with open(self.__meta_path, "r") as f:
+            data = json.load(f)
+            self.meta = MetaData(**data)
+
+    def load_qr(self, qr_data: BytesIO) -> None:
+        """
+        Write QR code data to a file.
+
+        Args:
+            qr_data (BytesIO): QR code image data in bytes.
+        """
+        self.__status[UserData.QR_FILE] = True
+        if os.path.exists(self.__qr_path):
+            os.remove(self.__qr_path)
+
+        with open(self.__qr_path, "wb") as f:
+            f.write(qr_data.getbuffer())
+
+    def load_image(self, image_data: UploadFile) -> None:
+        """
+        Write image data to a file.
+
+        Args:
+            image_data (UploadFile): Image file uploaded by the user.
+        """
+        self.__status[UserData.IMAGE_FILE] = True
+        if os.path.exists(self.__image_path):
+            os.remove(self.__image_path)
+
+        with open(self.__image_path, "wb") as f:
+            shutil.copyfileobj(image_data.file, f)
+
+    def load_model(self, model_data: UploadFile) -> None:
+        """
+        Write model data to a file.
+
+        Args:
+            model_data (UploadFile): Model file uploaded by the user.
+        """
+        self.__status[UserData.MODEL_FILE] = True
+        if os.path.exists(self.__model_path):
+            os.remove(self.__model_path)
+
+        with open(self.__model_path, "wb") as f:
+            shutil.copyfileobj(model_data.file, f)
+
+    def load_audio(self, audio_data: UploadFile) -> None:
+        """
+        Write audio data to a file.
+
+        Args:
+            audio_data (UploadFile): Audio file uploaded by the user.
+        """
+        self.__status[UserData.AUDIO_FILE] = True
+        if os.path.exists(self.__audio_path):
+            os.remove(self.__audio_path)
+
+        with open(self.__audio_path, "wb") as f:
+            shutil.copyfileobj(audio_data.file, f)
+
+    def load_param(self, param_data: dict) -> None:
+        """
+        Write parameter data to a JSON file.
+
+        Args:
+            param_data (dict): Parameter data in dictionary format.
+        """
+        self.__status[UserData.PARAM_FILE] = True
+        if os.path.exists(self.__param_path):
+            os.remove(self.__param_path)
+
+        with open(self.__param_path, "w") as f:
+            json.dump(param_data, f, indent=4)
